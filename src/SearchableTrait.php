f87c64a683ef21802c0bd6d7813dc77405441cac
jackleberry <jackiehannaford@yahoo.co.uk>
2016-09-27 21:18:50 +0900

    Added cast to text when building query to allow for searching against non text fields

--- src/SearchableTrait.php
+++ src/SearchableTrait.php
@@ -299,7 +299,7 @@ trait SearchableTrait
      */
     protected function getCaseCompare($column, $compare, $relevance) {
         if($this->getDatabaseDriver() == 'pgsql') {
-            $field = "LOWER(" . $column . ") " . $compare . " ?";
+            $field = "LOWER(" . $column . "::text) " . $compare . " ?";
             return '(case when ' . $field . ' then ' . $relevance . ' else 0 end)';
         }
 
